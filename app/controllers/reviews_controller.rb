aefcd64ee16b28694664f9e6fb35b30c635154f1
Anais Segall <anais.segall@gmail.com>
2023-05-20 22:23:48 +0200

    Rails yelp

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,34 @@
+class ReviewsController < ApplicationController
+
+  def index
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @reviews = @restaurant.reviews
+  end
+
+  def new
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = @restaurant.reviews.build
+  end
+
+  before_action :set_restaurant, only: %i[new create]
+
+  def create
+    @review = Review.new(review_params)
+    @review.restaurant = @restaurant
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
+  end
+
+  def review_params
+    params.require(:review).permit(:rating, :content)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+le_petit_parc = { name: 'Le Petit Parc', address: '14 Rue de Thann, 75017, Paris', phone_number: '0789887221', category: 'french' }
+pizza_east =  { name: 'Pizza East', address: "56A Shoreditch High St, London E1 6PQ", phone_number: '0102030405', category: 'french' }
+le_coude_fou = { name: 'Le Coude Fou', address: '12 rue du Bourg Tibourg, 75004 Paris', phone_number: '0142771518', category: 'italian' }
+little_cafe = { name: 'Little Café', address: '10 rue du Bourg Tibourg, 75004 Paris', phone_number: '0142771516', category: 'chinese' }
+la_menagerie = { name: 'La Ménagerie', address: 'Boulevard de l Hôpital, 75013 Paris', phone_number: '0142771538', category: 'chinese' }
+
+[le_petit_parc, pizza_east, le_coude_fou, little_cafe, la_menagerie].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
